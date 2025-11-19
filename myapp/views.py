e508804ed0bcd47aee167c1a0ad2327a6e16bcac
senior300 <seniorcouncil36@gmail.com>
2025-11-19 14:59:50 +0300

    first commit

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def gallery(request):
+    return render(request, 'gallery.html')
+
+def contacts(request):
+    return render(request, 'contacts.html')
